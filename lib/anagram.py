c4e64ca44345423434ee62e93401d29638fa6eb3
Gabriela Galvan <gabriela1galvan@gmail.com>
2023-03-16 12:42:24 -0700

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word_letters = sorted([letter for letter in word])
+    
+    def match(self, list):
+        new_list = []
+
+        for word in list:
+            if sorted([letter for letter in word]) == self.word_letters:
+                new_list.append(word)
+        
+        return new_list
